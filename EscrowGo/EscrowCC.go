a703f5b06c36fe288c18ca368c53a7403816490d
Khajuria <u358910@ent.wfb.bank.corp>
2017-08-30 11:11:37 +0530

    Merged branches

--- EscrowGo/EscrowCC.go
+++ EscrowGo/EscrowCC.go
@@ -363,7 +363,7 @@ var jsonResp string
 		return nil, errors.New(jsonResp)
 	}
 	docIndx, err = strconv.Atoi(string(docIndxData))
-	fmt.printf("In AllTransactions");
+	fmt.Printf("In AllTransactions");
 	var indxStart, indxEnd int
 	if pageNum > 0 {
 		indxStart = ((pageNum - 1) * pageSize) + 1
