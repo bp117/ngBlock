0555422284fece6ca25ce59faeeb3cb00d038776
Muthineni <u296305@ent.wfb.bank.corp>
2017-08-31 10:26:51 +0530

    Displaying state and county from DB

--- src/app/taxmanagement/insurenceForm.component.ts
+++ src/app/taxmanagement/insurenceForm.component.ts
@@ -1,28 +1,51 @@
 import {Component, Output,Input} from '@angular/core';
 import { NgForm }  from '@angular/forms';
+import { DataService } from '../shared/data.service';
 
 
 @Component({
   selector: 'display-insurance-modal',
-  templateUrl: 'insurenceForm.component.html'
+  templateUrl: 'insurenceForm.component.html',
+   providers: [DataService]
 })
 export class InsurenceFormComponent {
         
      @Output() states : any;
+         escrowDashboard:boolean = false;
+         messages: any = [];
+    counties : any =[];
+        
     
-        constructor() {
-             this.states = [{viewValue:"NJ"},{viewValue:"WV"},{viewValue:"MN"},{viewValue:"SD"},{viewValue:"OK"},{viewValue:"AK"},{viewValue:"AL"},
+    
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit Method");
+        this.dataService.getDocuments().then((data) => {
+                this.messages = data;
+             console.log("this.messages ",this.messages);  
+               this.counties = data[0].counties;
+            this.states = data[5].states;
+            console.log("this.counties ",this.counties); 
+             console.log("this.states ",this.states);  
+        }).catch((ex) => {
+                console.error('Error fetching users', ex);
+        });
+
+     }
+    
+        constructor(public dataService: DataService) {
+            /* this.states = [{viewValue:"NJ"},{viewValue:"WV"},{viewValue:"MN"},{viewValue:"SD"},{viewValue:"OK"},{viewValue:"AK"},{viewValue:"AL"},
                  {viewValue:"ND"},{viewValue:"WY"},{viewValue:"CA"},{viewValue:"MA"},{viewValue:"VA"},{viewValue:"WI"},{viewValue:"AR"},
                  {viewValue:"NM"},{viewValue:"MD"},{viewValue:"TN"},{viewValue:"OH"},{viewValue:"NE"},{viewValue:"NH"},{viewValue:"ME"},
                 {viewValue:"GA"},{viewValue:"IL"},{viewValue:"MI"},{viewValue:"KS"},{viewValue:"UT"},{viewValue:"NY"},{viewValue:"RI"},
                 {viewValue:"DE"},{viewValue:"DC"},{viewValue:"SC"},{viewValue:"IA"},{viewValue:"MT"},{viewValue:"CO"},{viewValue:"VT"},
                 {viewValue:"CT"},{viewValue:"FL"},{viewValue:"MS"},{viewValue:"IN"},{viewValue:"MO"},{viewValue:"LA"},{viewValue:"TX"},
                 {viewValue:"ID"},{viewValue:"WA"},{viewValue:"NV"},{viewValue:"HI"},{viewValue:"PA"},{viewValue:"NC"},{viewValue:"KY"},
-                {viewValue:"AZ"},{viewValue:"OR"}];
+                {viewValue:"AZ"},{viewValue:"OR"}];*/
              }
 
         ngOnInit(){
             this.tableVisible = false;
+            this.dataService.initCall();
             }
     
 
@@ -34,7 +57,7 @@ export class InsurenceFormComponent {
     
     
     
-    counties = [{viewValue:"GA"},{viewValue:"IL"},{viewValue:"MI"}];
+    //counties = [{viewValue:"GA"},{viewValue:"IL"},{viewValue:"MI"}];
     /*getCountyList(item : any){
         countiesList = [{viewValue: 'Savings', value: 'NJ'},
                     {viewValue: 'Basic Checking', value: 'WV'},
